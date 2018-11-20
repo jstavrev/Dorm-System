d511d561c33dfc5acd3dbeeb9eb2a1ce547df74e
Ioan Stavrev <j.stavrev@yahoo.com>
2018-11-20 13:27:51 +0200

    - removed unneeded namespace, refactoring

--- smartDormitory/SmartDormitory.Services/Contracts/IHomeService.cs
+++ smartDormitory/SmartDormitory.Services/Contracts/IHomeService.cs
@@ -3,7 +3,6 @@ using SmartDormitory.Models.Models;
 using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using X.PagedList;
 
 namespace SmartDormitory.Data.Services.Contracts
 {
