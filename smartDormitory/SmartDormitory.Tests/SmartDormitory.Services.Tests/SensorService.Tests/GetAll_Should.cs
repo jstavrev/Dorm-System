8e1903af447cb95ce3dec58be6dc47e4ab937f69
Ioan Stavrev <j.stavrev@yahoo.com>
2018-12-15 17:34:17 +0200

    - installed required nugets in test assembly

---
+++ smartDormitory/SmartDormitory.Tests/SmartDormitory.Services.Tests/SensorService.Tests/GetAll_Should.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SmartDormitory.Data.Data;
+
+namespace SmartDormitory.Tests.SmartDormitory.Services.Tests.SensorService.Tests
+{
+    [TestClass]
+    public class GetAll_Should
+    {
+        [TestMethod]
+        public void ReturnAllSensors_When_Invoked()
+        {
+            //Arrange
+            var contextOptions = new DbContextOptionsBuilder<SmartDormitoryDbContext>()
+               .UseInMemoryDatabase(databaseName: "ReturnActorAddedToDatabase_SuccessMessage")
+               .Options;
+
+            //Act
+
+            //Assert
+        }
+    }
+}
