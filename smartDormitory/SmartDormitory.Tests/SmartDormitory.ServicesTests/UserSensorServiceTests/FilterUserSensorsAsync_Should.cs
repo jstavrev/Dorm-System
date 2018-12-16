53b0e8bca130b641357df1cb61af8130c07da218
Alexander Stoychev <stoychev92@yahoo.co.uk>
2018-12-16 18:28:49 +0200

    added folder for user sensor service tests

---
+++ smartDormitory/SmartDormitory.Tests/SmartDormitory.ServicesTests/UserSensorServiceTests/FilterUserSensorsAsync_Should.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SmartDormitory.Data.Data;
+using SmartDormitory.Services.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace SmartDormitory.Tests.SmartDormitory.ServicesTests.UserSensorServiceTests
+{
+    [TestClass]
+    public class FilterUserSensorsAsync_Should
+    {
+        [TestMethod]
+        public async Task ThrowArgumentNullException_WhenPassedNullSortOrder()
+        {
+            // Arrange
+            Mock<SmartDormitoryDbContext> ContextMock = new Mock<SmartDormitoryDbContext>();
+
+            string userId = null;
+            string invalidSortOrder = string.Empty;
+            string validFilter = string.Empty;
+            int validPageNumber = 1;
+            int validPageSize = 10;
+
+            UserSensorService SUT = new UserSensorService(
+                ContextMock.Object);
+
+            // Act & Assert
+            await Assert.ThrowsExceptionAsync<ArgumentNullException>(
+                () => SUT.FilterUserSensorsAsync(userId, invalidSortOrder, validFilter, validPageNumber, validPageSize));
+        }
+    }
+}
